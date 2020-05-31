c4503419164f5b3f7b978293473fc1e05d85caf1
Mick Byrne <phpmick@gmail.com>
2020-05-31 17:47:29 +0100

    Adding files for books

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', 'AuthController@login');
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/users', 'UserController@index');
+    Route::get('/books', 'BookController@index');
 });
 
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
